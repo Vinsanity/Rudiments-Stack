52377ed1ff6eb6e776cd673e4340f59f5092fe3c
Vincent Listrani <vincent@3five.com>
2015-06-16 12:16:28 -0500

    Fixed issue with the wp branch submodule not checking out the correct branch.

--- config/prepare.sh
+++ config/prepare.sh
@@ -1,7 +1,7 @@
 rm -rf .git
 git init
 rm -rf wp
-git submodule add -b 4.2.2-branch https://github.com/WordPress/WordPress.git wp
+git submodule add -b 4.2-branch https://github.com/WordPress/WordPress.git wp
 git remote rm origin
 git add -A
 git commit -m "Inital commit"
