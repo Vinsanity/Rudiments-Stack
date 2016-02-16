e38ac383ca0d50785181c054633d39c28bcdc7fc
Jon Jaques <jon@3five.com>
2016-02-16 14:47:51 -0600

    Add browser-sync functionality to build system

--- build/tasks/dev.js
+++ build/tasks/dev.js
@@ -1,13 +1,26 @@
 import gulp from 'gulp'
 import path from 'path'
+import BrowserSync from 'browser-sync'
 import { compiler, handleWebpackResults } from '../webpack/compiler'
 
+const browserSync = BrowserSync.create()
 const args = global.__args
 const themeDir = path.resolve(__pkg._themepath)
+const themeRelPath = themeDir.replace(process.cwd()+'/', '')
 
-gulp.task('dev', ['styles'], ()=> {
+gulp.task('dev', ['build'], ()=> {
   compiler.watch({}, handleWebpackResults(true))
-  gulp.watch(`${themeDir}/scss/**/*.scss`, ['styles'])
-  gulp.watch(`${__pkg._themepath}/images/**/*`, ['images'])
-  gulp.watch(`${themeDir}/fonts/**/*`, ['static'])
+  gulp.watch(`${themeRelPath}/scss/**/*.scss`, ['styles'])
+  gulp.watch(`${themeRelPath}/images/**/*`, ['images'])
+  gulp.watch(`${themeRelPath}/fonts/**/*`, ['static'])
+
+  if (args.sync) {
+    browserSync.init({
+      proxy: __pkg._criticalUrl,
+      files: [
+        `${themeRelPath}/assets/js/*.js`,
+        `${themeRelPath}/**/*.php`
+      ]
+    })
+  }
 })
