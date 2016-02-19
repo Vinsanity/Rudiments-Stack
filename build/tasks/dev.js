dc17c5e8a4a864c49c5ff167a8badd524db18da5
Jon Jaques <jon@3five.com>
2016-02-19 10:22:34 -0600

    Slim down the variables

--- build/tasks/dev.js
+++ build/tasks/dev.js
@@ -5,22 +5,20 @@ import { compiler, handleWebpackResults } from '../webpack/compiler'
 
 const browserSync = BrowserSync.create()
 const args = global.__args
-const themeDir = path.resolve(__pkg._themepath)
-const themeRelPath = themeDir.replace(process.cwd()+'/', '')
 
 gulp.task('dev', ['build'], ()=> {
   compiler.watch({}, handleWebpackResults(true))
-  gulp.watch(`${themeRelPath}/scss/**/*.scss`, ['styles'])
-  gulp.watch(`${themeRelPath}/images/**/*`, ['images'])
-  gulp.watch(`${themeRelPath}/fonts/**/*`, ['static'])
+  gulp.watch(`${__pkg._themepath}/scss/**/*.scss`, ['styles'])
+  gulp.watch(`${__pkg._themepath}/images/**/*`, ['images'])
+  gulp.watch(`${__pkg._themepath}/fonts/**/*`, ['static'])
 
   if (args.sync) {
     browserSync.init({
       proxy: __pkg._criticalUrl,
       files: [
-        `${themeRelPath}/assets/js/*.js`,
-        `${themeRelPath}/assets/css/*.css`,
-        `${themeRelPath}/**/*.php`
+        `${__pkg._themepath}/assets/js/*.js`,
+        `${__pkg._themepath}/assets/css/*.css`,
+        `${__pkg._themepath}/**/*.php`
       ]
     })
   }
