fbeb71db616af0925421dda5f7ea862fb7a29eb3
Vinny Listrani <vincent@3five.com>
2016-08-22 14:25:40 -0500

    Updated Capistrano and friends ruby gems.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source 'https://rubygems.org'
 
-gem 'capistrano', '~> 3.0'
+gem 'capistrano', '~> 3.6.0'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for Capistrano 3.1
-lock '3.1.0'
+lock '3.6.0'
 
 # Uncomment this line if you're using vagrant as your localhost
 # set :vagrant_local, true
